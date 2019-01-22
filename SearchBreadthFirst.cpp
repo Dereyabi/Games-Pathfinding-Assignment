d3adc6501028b98818a2d2f4f7c7b19007fe88ec
Dereyabi <37939640+Dereyabi@users.noreply.github.com>
2019-01-22 14:37:18 +0000

    Breadth First completely working, please god dont touch it. its a fragile being
    need to do:
    UI
    A* algorithm
    Dijkstra's
    visual representation of a path
    choosing map file
    change maps whilst program is running

--- SearchBreadthFirst.cpp
+++ SearchBreadthFirst.cpp
@@ -18,63 +18,66 @@ bool CSearchBreadthFirst::FindPath(TerrainMap& terrain, unique_ptr<SNode>& start
 	unique_ptr <SNode> temp;
 	unique_ptr <SNode> currentNode;
 
-	bool found = false;
+	
 
 	openList.push_back(move(start));
 
-
+	bool found = false;
 	
 
-	while(!openList.empty())
+	while(!openList.empty() && found == false)
 	{
 
 		currentNode = move(openList.front());
 		openList.pop_front();
 
-		if (currentNode->x == goal->x && currentNode->y == goal->y)
+		if (currentNode->x == goal->y && currentNode->y == goal->x)
 		{
 			found = true;
 		}
 
-		//North
-		if (!ListSearch(openList, currentNode->x, currentNode->y + 1) && !ListSearch(closedList, currentNode->x, currentNode->y + 1) && terrain[currentNode->x][currentNode->y + 1] != Wall)					//wall check needs + or - on current node depending on direction 
-		{
-			temp.reset(new SNode);
-			temp->x = currentNode->x;
-			temp->y = currentNode->y + 1;
-			cout << currentNode->x << ", " << currentNode->y << endl;
-			openList.push_back(move(temp));
-		}
 
-		//East
+		//North
 		if (!ListSearch(openList, currentNode->x + 1, currentNode->y) && !ListSearch(closedList, currentNode->x + 1, currentNode->y) && terrain[currentNode->x + 1][currentNode->y] != Wall)					//wall check needs + or - on current node depending on direction 
 		{
 			temp.reset(new SNode);
 			temp->x = currentNode->x + 1;
 			temp->y = currentNode->y;
-			cout << currentNode->x << ", " << currentNode->y << endl;
+			cout << currentNode->y << ", " << currentNode->x << endl;
 			openList.push_back(move(temp));
 		}
 
-		//South
-		if (!ListSearch(openList, currentNode->x, currentNode->y - 1) && !ListSearch(closedList, currentNode->x, currentNode->y - 1) && terrain[currentNode->x][currentNode->y - 1] != Wall)					//wall check needs + or - on current node depending on direction 
+		//East
+		if (!ListSearch(openList, currentNode->x, currentNode->y + 1) && !ListSearch(closedList, currentNode->x, currentNode->y + 1) && terrain[currentNode->x][currentNode->y + 1] != Wall)					//wall check needs + or - on current node depending on direction 
 		{
 			temp.reset(new SNode);
 			temp->x = currentNode->x;
-			temp->y = currentNode->y - 1;
-			cout << currentNode->x << ", " << currentNode->y << endl;
+			temp->y = currentNode->y + 1;
+			cout << currentNode->y << ", " << currentNode->x << endl;
 			openList.push_back(move(temp));
 		}
-
-		//West
+		
+		//South
 		if (!ListSearch(openList, currentNode->x - 1, currentNode->y) && !ListSearch(closedList, currentNode->x - 1, currentNode->y) && terrain[currentNode->x - 1][currentNode->y] != Wall)					//wall check needs + or - on current node depending on direction 
 		{
 			temp.reset(new SNode);
 			temp->x = currentNode->x - 1;
 			temp->y = currentNode->y;
-			cout << currentNode->x << ", " << currentNode->y << endl;
+			cout << currentNode->y << ", " << currentNode->x << endl;
 			openList.push_back(move(temp));
 		}
+
+		//West
+		if (!ListSearch(openList, currentNode->x, currentNode->y - 1) && !ListSearch(closedList, currentNode->x, currentNode->y - 1) && terrain[currentNode->x][currentNode->y - 1] != Wall)					//wall check needs + or - on current node depending on direction 
+		{
+			temp.reset(new SNode);
+			temp->x = currentNode->x;
+			temp->y = currentNode->y - 1;
+			cout << currentNode->y << ", " << currentNode->x << endl;
+			openList.push_back(move(temp));
+		}
+
+		
 
 		closedList.push_back(move(currentNode));
 	}
